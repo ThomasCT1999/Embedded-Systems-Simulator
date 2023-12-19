ead70ce830872811540095208bff24692ba41151
ThomasCT1999 <123325434+ThomasCT1999@users.noreply.github.com>
2023-12-19 21:00:50 +0000

    new

--- main.c
+++ main.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 #include <time.h>
 
-// Function to simulate temperature sensor data
 float simulateTemperatureSensor() {
-    // Generating a random temperature between -10.0 and 35.0 degrees
     float temp = (rand() % 460 - 100) / 10.0;
     return temp;
 }
 
 int main() {
-    // Initialize random number generator
     srand(time(NULL));
-
     printf("Embedded Systems Simulator: Temperature Sensor\n");
 
     while (1) {
         float temperature = simulateTemperatureSensor();
         printf("Current Temperature: %.1f°C\n", temperature);
-
-        // Delay for a bit (simulating sensor read interval)
-        sleep(1);
+        fflush(stdout);  // Flush the output buffer after printing
+        Sleep(1000);
     }
 
     return 0;
